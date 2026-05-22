1323f59b0f84ab752e99d6a29d2fad708ad413e0
amazigh233 <bilal.zambib4@gmail.com>
2026-05-23 01:17:50 +0200

    Add EF Core infrastructure and database setup

--- src/Booking.Infrastructure/Migrations/BookingDbContextModelSnapshot.cs
+++ src/Booking.Infrastructure/Migrations/BookingDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace Booking.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.8")
+                .HasAnnotation("ProductVersion", "9.0.16")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
